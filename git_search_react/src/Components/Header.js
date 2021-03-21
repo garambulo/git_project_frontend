13e307b170711d977c3ed6879e8e20e9c165ef61
garambulo <genrev_arambulo009@yahoo.com>
2021-03-21 16:00:10 +0800

    Pushed base project
    Added files for the initial react project

---
+++ git_search_react/src/Components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class header extends Component {
+    render() {
+        return (
+            <header>
+                This is my header
+            </header>
+        )
+    }
+}
