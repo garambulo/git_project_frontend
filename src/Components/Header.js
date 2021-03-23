625bc8ae01a6d9db05d93d09bb03ef2430297e99
garambulo <genrev_arambulo009@yahoo.com>
2021-03-24 00:42:43 +0800

    Clickable Header
    Changed the header to link component that redirects to home page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 import 'antd/dist/antd.css';
 import { Layout, Input, Row, Col } from 'antd';
 import { GithubOutlined } from '@ant-design/icons';
+import { Link } from "react-router-dom";
 const { Header } = Layout;
 const { Search } = Input;
+
 
 
 export default class header extends Component {
@@ -13,7 +15,11 @@ export default class header extends Component {
             <Header>
                 <div className="logo" />
                     <Row>
-                        <Col span={6}><GithubOutlined />&nbsp; GitHub Project</Col>
+                        <Col span={6}>
+                            <GithubOutlined />
+                            &nbsp;
+                            <Link to="/">GitHub Project</Link>
+                        </Col>
                         <Col span={6}> </Col>
                         <Col span={6}></Col>
                         <Col span={6}><Search placeholder="input search text" onSearch={this.props.onSearch} enterButton /> </Col>
